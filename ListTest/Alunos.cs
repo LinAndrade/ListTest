782537876e0134eb6ed504770713785cb80b2d74
Lincoln Andrade <lincoln.andrade@uel.br>
2023-03-29 04:41:48 -0300

    commit inicial

---
+++ ListTest/Alunos.cs
@@ -0,0 +1,8 @@
+﻿
+
+public class Alunos
+{
+    public string nome { get; set; } = default;
+    public string ano { get; set; } = default;
+    public string matéria { get; set; } = default;
+}
---
+++ ListTest/Program.cs
@@ -0,0 +1,23 @@
+﻿List<Alunos> lstAlunos = new List<Alunos>();
+
+for (int i = 0; i < 3; i++)
+{
+    Alunos alunos = new Alunos();
+    Console.WriteLine("Nome do aluno:");
+    alunos.nome = Console.ReadLine();
+    Console.WriteLine("Ano do aluno:");
+    alunos.ano = Console.ReadLine();
+    Console.WriteLine("Matéria favorita do aluno:");
+    alunos.matéria = Console.ReadLine();
+
+    lstAlunos.Add(alunos);
+    Console.WriteLine("====================");
+}
+
+Console.WriteLine("====================");
+Console.WriteLine("Lista de alunos");
+Console.WriteLine("====================");
+foreach (var alunos in lstAlunos)
+{
+    Console.WriteLine("Nome: " + alunos.nome + " |Ano: " + alunos.ano + " |Matéria favorita: " + alunos.matéria);
+}
